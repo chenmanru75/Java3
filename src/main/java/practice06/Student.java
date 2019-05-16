3b9c37a635359fb3b4b7bc9e7c0342ba56ba2a02
chenmanru75 <chenmanru75@163.com>
2019-05-16 20:26:44 +0800

    practice06 ok

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,16 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+    private Integer klass;
+    private String str = " I am a Student. I am at Class 2.";
+    public Student(String name,Integer age,Integer klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public String introduce(){
+        return super.introduce()+str;
+    }
+    public Integer getKlass(){
+        return klass;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,27 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Integer klass;
+    private String str = " I am a Teacher. I teach Class 2.";
+    private String str2 = " I am a Teacher. I teach No Class.";
+    public Teacher(String name,Integer age,Integer klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public Teacher(String name,Integer age){
+        super(name,age);
+    }
+    private String s;
+    public String introduce(){
+        if(this.getKlass() == null){
+            s = str2;
+        }else{
+            s = str;
+        }
+        return super.introduce() + s;
+    }
+
+    public Integer getKlass(){
+        return klass;
+    }
 }
