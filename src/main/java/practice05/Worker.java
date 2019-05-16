5bfa263d1395f92e6e5304f457decb254d8c029d
chenmanru75 <chenmanru75@163.com>
2019-05-16 20:00:10 +0800

    practice05 ok

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -1,4 +1,12 @@
 package practice05;
 
-public class Worker {
+public class Worker extends Person{
+    private String str = " I am a Worker. I have a job.";
+    public Worker(String name,Integer age){
+        super(name,age);
+        super.introduce();
+    }
+    public String introduce(){
+        return super.introduce()+str;
+    }
 }
