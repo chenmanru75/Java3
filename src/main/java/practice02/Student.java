e16e431e670acd96b13c09fe627b5caa40ec536e
chenmanru75 <chenmanru75@163.com>
2019-05-16 18:57:15 +0800

    practice02 ok

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,15 @@
 package practice02;
 
-public class Student {
+public class Student extends Person {
+     private Integer klass;
+    public Student(String name,Integer age,Integer klass){
+        super(name,age);
+        this.klass = klass;
+    }
+    public Integer getKlass(){
+        return klass;
+    }
+    public String introduce(){
+        return "I am a Student. I am at Class 2.";
+    }
 }
