d3de2f5d7b5bca23b8612ebbd00030b14d8206cd
chenmanru75 <chenmanru75@163.com>
2019-05-17 20:49:49 +0800

    practice10 未完成（还剩下一个）

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,31 @@
 package practice10;
 
 public class Klass {
+    private Integer klass;
+    public Klass(Integer klass){
+        this.klass = klass;
+    }
+    public Integer getNumber(){
+        return klass;
+    }
+    public String getDisplayName(){
+        return "Class 2";
+    }
+    Student student;
+    public int flag = 0;
+    public void assignLeader(Student student){
+        if(flag2 == 1){
+            this.flag = 1;
+            this.student = student;
+        }else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+    public Student getLeader(){
+        return student;
+    }
+    private int flag2 = 0;
+    public void appendMember(Student student){
+        flag2 = 1;
+    }
 }
